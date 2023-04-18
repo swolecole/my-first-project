9d6b7d957e52ea83db1f24dcdcb02f22bccd4e9c
unknown <cole.winston.james@gmail.com>
2023-04-18 17:44:06 -0600

    update commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Guid } from "guid-typescript";
+import { Todo } from "src/models/todo.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppCompenet {
+  todos: Todo[] = []
+
+  onSubmit(form: NgForm){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    todo.isComplete = true;
+  }
+
+  onDelete(id: Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0];
+    let index = this.todos.indexOf(todo, 0);
+    if(index > -1){
+      this.todos.splice(index,1);
+    }
+  }
 }
